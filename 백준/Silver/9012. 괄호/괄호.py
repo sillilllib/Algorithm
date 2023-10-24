6d5164e2dce9d52d7258eea77eb38cea7925eec3
SU BEEN <127243183+sillilllib@users.noreply.github.com>
2023-10-25 05:42:27 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,19 @@
+N = int(input())
+
+for i in range(N):
+  s = list(input())
+  sum = 0
+
+  for i in s:
+    if i == '(':
+      sum += 1
+    elif i == ')':
+      sum -= 1
+    if sum < 0:
+      print('NO')
+      break
+
+  if sum > 0:
+    print('NO')
+  elif sum == 0:
+    print('YES')
